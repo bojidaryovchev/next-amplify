208a877647f4021910ffe6669c6508c661651740
bojidaryovchev <blizzardcon10@gmail.com>
2021-11-21 16:18:43 +0200

    fix for the Object literal may only specify known properties, and 'authorize' does not exist in type 'NexusOutputFieldConfig<"Query", "accounts">' error https://github.com/graphql-nexus/nexus/issues/511

--- next-env.d.ts
+++ next-env.d.ts
@@ -4,19 +4,3 @@
 
 // NOTE: This file should not be edited
 // see https://nextjs.org/docs/basic-features/typescript for more information.
-
-declare global {
-  interface NexusGenPluginTypeConfig<TypeName extends string> {}
-  interface NexusGenPluginFieldConfig<TypeName extends string, FieldName extends string> {
-    /**
-     * Authorization for an individual field. Returning "true"
-     * or "Promise<true>" means the field can be accessed.
-     * Returning "false" or "Promise<false>" will respond
-     * with a "Not Authorized" error for the field.
-     * Returning or throwing an error will also prevent the
-     * resolver from executing.
-     */
-    authorize?: FieldAuthorizeResolver<TypeName, FieldName>;
-  }
-  interface NexusGenPluginSchemaConfig {}
-}
